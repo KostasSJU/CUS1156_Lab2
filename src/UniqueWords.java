4664ce8901d5f248253d01c72f85172163cde34f
Kostas Georgiou <konstantinos.georgiou20@my.stjohns.edu>
2022-02-16 12:50:10 -0500

    Completed assignemnt

--- src/UniqueWords.java
+++ src/UniqueWords.java
@@ -1,3 +1,10 @@
+// Lab 2 - Git
+// UniqueWords.java
+// Konstantinos Georgiou
+// 2/10/22
+// This is lab 2 where we will clone a project to our local machine
+// edit it, and upload it back to github.
+
 import java.util.ArrayList;
 
 public class UniqueWords
@@ -10,12 +17,36 @@ public class UniqueWords
    public static int countUnique(ArrayList<String> list)
    {
 	  int count = 0;
-	  
+
       for (int i = 0; i < list.size(); i++)
-      {		 for (int j = 0; j < list.size(); j++)
+      {		
+    	 for (int j = 0; j < list.size(); j++)
 		 {
-			
+    		 
+    	/*  	  
+    	  long unique = list.stream()
+                  .distinct()
+                  .count();
+    	  System.out.println(unique);
+    	  */ 
+    	  if(j < i && list.get(j) == list.get(i))
+    	  {
+    		  break;
+    	  }
+    	  else if(j > i && list.get(j) != list.get(i))
+    	  {
+    		  count ++; 
+    		  break;
+    		  
+    	  }
+    	  else if(j == list.size() -1)
+    	  {
+    		  count++;
+    		  		  
+    	  }
+    	  
 		 }
+		 
       }
 	  return count;
    }
@@ -30,6 +61,7 @@ public class UniqueWords
 	  words.add("orange");
 	  words.add("sun");
 	  words.add("moon");
+	  
    
 	  int unique = countUnique(words);
       System.out.println(words + " has " + unique + " unique words");
